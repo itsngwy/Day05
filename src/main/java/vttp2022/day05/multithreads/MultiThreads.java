a6af7b187d60d304685bf41ee42c4a1bcf375794
YangTeng <ngwy17@gmail.com>
2022-12-20 14:55:31 +0800

    remove library

--- src/main/java/vttp2022/day05/multithreads/MultiThreads.java
+++ src/main/java/vttp2022/day05/multithreads/MultiThreads.java
@@ -4,7 +4,6 @@ import java.security.SecureRandom;
 import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 
 // Done in day07
 // Creating MultiThreads
